beb655e69843f87ac47852030436fdfe3b63387d
Tsotne Putkaradze <tsotnep@gmail.com>
2016-07-18 13:48:21 +0400

    added print, that prints count of changes in a file

--- replace_a_line_in_file_that_starts_with_smth_with_smth_else.py
+++ replace_a_line_in_file_that_starts_with_smth_with_smth_else.py
@@ -14,17 +14,20 @@ import sys
 
 # a function that replaces a line that starts with : CHANGE_LINE_STARTING_WITH with a line : WITH_THIS_NEW_COMMAND, and takes FILENAME as argument
 def REPLACE_WITH_NEW_COMMAND (filename):
+    count = 0
     for line in fileinput.input(filename, inplace=True):
         if line.strip().startswith(CHANGE_LINE_STARTING_WITH):
             print WITH_THIS_NEW_COMMAND
+            count+=1
         else:
             line = line.rstrip('\n')
             print line
+    print ("in file: \"" + filename + "\" changed that many: " + str(count))
 
 #if you give no parameter, this will execute
 if len(sys.argv) == 1:
-    CHANGE_LINE_STARTING_WITH='asd'
-    WITH_THIS_NEW_COMMAND='HEY'
+    CHANGE_LINE_STARTING_WITH='XXXX'
+    WITH_THIS_NEW_COMMAND='YYYY'
     for v in ["CV_INT","CV_REAL", "S_INT", "S_INT_G", "S_REAL"]:
         REPLACE_WITH_NEW_COMMAND("tb/TB_MATRIX_MUL_IP_CORE_"+v+".vhd")
 
@@ -40,9 +43,9 @@ if len(sys.argv) == 3:
 
 #if you give four parameters, first will be name of the file
 if len(sys.argv) == 4:
-    REPLACE_WITH_NEW_COMMAND(sys.argv[1])
     CHANGE_LINE_STARTING_WITH=sys.argv[2]
     WITH_THIS_NEW_COMMAND=sys.argv[3]
+    REPLACE_WITH_NEW_COMMAND(sys.argv[1])
 
 #example of for loops:
 
